3f56c8d2ebce7baf34415bf1fbcf0bb613c5253f
annasvst <annaatalay@hotmail.com>
2026-02-04 20:06:53 +0100

    Add Lesson22 completed files

---
+++ Lesson22/script.js
@@ -0,0 +1,68 @@
+const products = {
+  apples: {
+    quantity: 1,
+    price: 1,
+  },
+  bananas: {
+    quantity: 1,
+    price: 1,
+  },
+  bread: {
+    quantity: 1,
+    price: 1,
+  },
+  eggs: {
+    quantity: 1,
+    price: 1,
+  },
+};
+
+let totalPrice = 0;
+
+const totalPriceElement = document.getElementById('total');
+
+document.addEventListener('DOMContentLoaded', function () {
+  for (const product in products) {
+    totalPrice += products[product].quantity * products[product].price;
+    const productQuantitySpan = document.getElementById(`${product}_quantity`);
+    productQuantitySpan.textContent = products[product].quantity;
+
+    const addCartButton = document.getElementById(`${product}_add`);
+    addCartButton.addEventListener('click', () => addToCart(product));
+
+    const removeCartButton = document.getElementById(`${product}_remove`);
+    removeCartButton.addEventListener('click', () => removeFromCart(product));
+
+    // Select increment / decrement buttons for every product and add event listeners to them
+  }
+
+  totalPriceElement.textContent = totalPrice;
+});
+
+function addToCart(product) {
+  products[product].quantity++;
+  const newQuantity = products[product].quantity;
+
+  totalPrice += products[product].price;
+  totalPriceElement.textContent = totalPrice;
+
+  const productQuantitySpan = document.getElementById(`${product}_quantity`);
+  productQuantitySpan.textContent = newQuantity;
+
+  const productCartItem = document.getElementById(`${product}_cart`);
+  productCartItem.classList.remove('hidden');
+}
+
+function removeFromCart(product) {
+  const oldQuantity = products[product].quantity;
+
+  products[product].quantity = 0;
+
+  totalPrice -= products[product].price * oldQuantity;
+  totalPriceElement.textContent = totalPrice;
+
+  const productCartItem = document.getElementById(`${product}_cart`);
+  productCartItem.classList.add('hidden');
+}
+
+
